9121c04129cabd3350c242ca5dd4c5a4ce4e110e
caihaiyang <oceancai@163.com>
2021-07-16 10:59:26 +0800

    1.5

--- framelibrary/src/main/java/com/white/luckcloud/framelibrary/http/HttpRequestUtils.java
+++ framelibrary/src/main/java/com/white/luckcloud/framelibrary/http/HttpRequestUtils.java
@@ -20,11 +20,11 @@ public class HttpRequestUtils {
     }
 
     // 线上环境
-    private static final String SERVICE_ADDRESS_BASE_ONLINE = "http://47.242.255.32:8080/user";
+    private static final String SERVICE_ADDRESS_BASE_ONLINE = "http://8.212.12.209:8080/user";
     // 测试环境
-    private static final String SERVICE_ADDRESS_BASE_TEST = "http://47.242.255.32:8080/user";
+    private static final String SERVICE_ADDRESS_BASE_TEST = "http://8.212.12.209:8080/user";
     //图片文件前缀
-    public static final String SERVICE_ADDRESS_IMAGE = "http://47.242.255.32:8888/";
+    public static final String SERVICE_ADDRESS_IMAGE = "http://8.212.12.209:8888/";
 
     /**
      * 根据是否Debug模式获取地址前缀
